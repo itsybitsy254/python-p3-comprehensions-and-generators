3a588a5e46a5d8ee04b92cc111f7d42c54021071
Vitalis Ian <viterlis254@gmail.com>
2024-08-30 13:02:17 +0300

    initial

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """Returns a list of even numbers from num_list. If no evens, returns an empty list."""
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """Appends an exclamation mark to each sentence in sentence_list. Returns the modified list."""
+    return [sentence + '!' for sentence in sentence_list]
